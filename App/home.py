30447c5aa7ce623577062704246ee86126a29932
Penzragon <rifkyaliffa@gmail.com>
2022-02-10 09:51:35 +0700

    Reorder the components

--- App/home.py
+++ App/home.py
@@ -10,11 +10,6 @@ def app():
         )
 
         st.markdown(
-            "<p style='text-align: center;'>Made with <span style='color: red'>♥️</span> by <b>Rifky Aliffa</b></p>",
-            unsafe_allow_html=True,
-        )
-
-        st.markdown(
             "<p style='text-align: center;'>This simple app is a project about data visualization dashboard and hypothesis testing where you can get an insight from a data. The dataset used in this project is a supermarket sales dataset which has recorded in 3 different branches for 3 months data. Dataset can be downloaded from <a href='https://www.kaggle.com/aungpyaeap/supermarket-sales'>Kaggle</a>.</p>",
             unsafe_allow_html=True,
         )
@@ -34,3 +29,8 @@ def app():
                 "<img src='https://media3.giphy.com/media/l0amJzVHIAfl7jMDos/giphy.gif?cid=ecf05e47wn6qux56otl3dc50n4yn4d4kegpf4woknc2m5d0y&rid=giphy.gif&ct=g' width='100%'/>",
                 unsafe_allow_html=True,
             )
+
+        st.markdown(
+            "<p style='text-align: center;'>Made with <span style='color: red'>♥️</span> by <b>Rifky Aliffa</b></p>",
+            unsafe_allow_html=True,
+        )
